07454119cb9219d34b7c0b3a7fb3c1f9dc74b7ec
daniel.colina <daniel.colina@dotcms.com>
2023-10-27 14:17:20 +0200

    feat: Testing snapshot tag.

--- src/main/java/com/dotcms/GreetingCommand.java
+++ src/main/java/com/dotcms/GreetingCommand.java
@@ -1,10 +1,12 @@
 package com.dotcms;
 
+import io.quarkus.runtime.annotations.QuarkusMain;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
 @Command(name = "greeting", mixinStandardHelpOptions = true)
+@QuarkusMain
 public class GreetingCommand implements Runnable {
 
     @Parameters(paramLabel = "<name>", defaultValue = "picocli",
@@ -16,4 +18,9 @@ public class GreetingCommand implements Runnable {
         System.out.printf("Hello %s, go go commando!\n", name);
     }
 
+    public static void main(String[] args) {
+        CommandLine.run(new GreetingCommand(), args);
+//        int exitCode = new CommandLine(new GreetingCommand()).execute(args);
+//        System.exit(exitCode);
+    }
 }
